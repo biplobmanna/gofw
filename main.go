4ff13e09cbc6ea49d3ac8bb1c45ca9dcf4596e6b
biplobmanna <biplobmanna@outlook.com>
2026-03-25 05:45:54 +0530

    update: recursive filepath watch
    walk the filepath, and add any dir to be watched

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
 
 	"github.com/fsnotify/fsnotify"
 )
 
 func main() {
+	// path to watch
+	path := "/home/nomana/Github/gofw"
+
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -21,8 +29,18 @@ func main() {
 				if !ok {
 					return
 				}
-				if event.Has(fsnotify.Write) || event.Has(fsnotify.Create) || event.Has(fsnotify.Remove) || event.Has(fsnotify.Remove) {
-					log.Printf("File Modified: %v", event.Name)
+				if event.Has(fsnotify.Write) || event.Has(fsnotify.Remove) {
+					log.Printf("%v: %v", event.Op.String(), event.Name)
+				} else if event.Has(fsnotify.Create) {
+					log.Printf("%v: %v", event.Op.String(), event.Name)
+					info, err := os.Stat(event.Name)
+					if err != nil {
+						log.Fatal(err)
+					} else if info.IsDir() {
+						log.Printf("Watching New Folder: %v\n", event.Name)
+						watchRecursive(event.Name, watcher)
+					}
+
 				}
 			case err, ok := <-watcher.Errors:
 				if !ok {
@@ -33,15 +51,28 @@ func main() {
 		}
 	}()
 
-	// path to watch
-	path := "."
-	err = watcher.Add(path)
+	err = watchRecursive(path, watcher)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("watching for changes in: %v", path)
 
 	// block the main gorouting forever
-	<-make(chan struct{})
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	<-sig
+	fmt.Println("shutting down gracefully...")
 
+}
+
+func watchRecursive(path string, watcher *fsnotify.Watcher) error {
+	return filepath.Walk(path, func(newPath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return watcher.Add(newPath)
+		}
+		return nil
+	})
 }
